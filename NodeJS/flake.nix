f560cfd9cb4f7c3f9ab1ac01f1ad7a66244a418b
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-04-17 00:20:29 +0700

    improve: update flake

--- NodeJS/flake.nix
+++ NodeJS/flake.nix
@@ -9,17 +9,16 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
 
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
+      let pkgs = nixpkgs.legacyPackages.${system};
 
-      {
+      in {
 
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = with pkgs; [
             nodejs
             nodePackages.yarn
+            nodePackages.pnpm
             nodePackages.prettier
             nodePackages.typescript
             nodePackages.prisma
