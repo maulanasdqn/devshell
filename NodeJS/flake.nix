b45fc349e113137a112a5dbef2a0666f908aa0eb
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-09-11 10:27:07 +0700

    feature: upgrade dependency

--- NodeJS/flake.nix
+++ NodeJS/flake.nix
@@ -1,38 +1,34 @@
 {
-  description = "NodeJS Dev Shell";
+  description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils }:
 
-      let pkgs = nixpkgs.legacyPackages.${system};
+    utils.lib.eachDefaultSystem (system:
+      let
+        nodejsVersion = 18;
+        overlays = [
+          (final: prev: rec {
+            nodejs = prev."nodejs-${toString nodejsVersion}_x";
+            pnpm = prev.nodePackages.pnpm;
+            yarn = (prev.yarn.override { inherit nodejs; });
+          })
+        ];
+
+        pkgs = import nixpkgs { inherit overlays system; };
 
       in {
-
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ pkgs.bashInteractive ];
-          buildInputs = with pkgs; [
-            nodejs
-            nodePackages.yarn
-            nodePackages.pnpm
-            nodePackages.prettier
-            nodePackages.typescript
-            nodePackages.prisma
-            openssl
-          ];
+          buildInputs = with pkgs; [ nodejs pnpm yarn ];
+
           shellHook = with pkgs; ''
-            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-            export PATH=~/.npm-packages/bin:$PATH
-            export NODE_PATH=~/.npm-packages/lib/node_modules
-            echo "Anda menggunakan NodeJS versi $(node -v)"
+            echo "node `${nodejs}/bin/node --version`"
+            echo "yarn `${yarn}/bin/yarn --version`"
+            echo "pnpm `${pnpm}/bin/pnpm --version`"
           '';
         };
 
